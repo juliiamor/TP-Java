ad4392ab4fec700ec517b6bcb887d7f877629e1d
juliiamor <juliiamor17@gmail.com>
2024-10-17 17:17:56 -0300

    cambio nombre id

--- src/Discográfica/Artista.java
+++ src/Discográfica/Artista.java
@@ -4,7 +4,7 @@ import
 import java.util.List;
 
 public abstract class Artista {
-    private String identificador;
+    private String id;
     private String nombre;
     private int cantIntegrantes;
     private String generoMusical;
