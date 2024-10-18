f77fcafb1d6ae5068a3b67278b2931a6486edf46
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-18 14:38:09 -0300

    Añado Constructor

--- src/Discográfica/ArtistaEmergente.java
+++ src/Discográfica/ArtistaEmergente.java
@@ -1,4 +1,7 @@
 package Discográfica;
 
 public class ArtistaEmergente extends Artista{
+    public ArtistaEmergente(String identificador,String nombre,byte cantIntegrantes,String generoMusical){
+        super(identificador, nombre, cantIntegrantes, generoMusical);
+    }
 }
