ca8c873fa0246a760faf47e42f094f1f4e13ea51
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-18 14:39:59 -0300

    Añado métodos y cambio contenedor canciones a Set

--- src/Discográfica/Disco.java
+++ src/Discográfica/Disco.java
@@ -1,8 +1,46 @@
 package Discográfica;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Disco {
-    private long int unidadesVendidasUltMes;
-    private List<Cancion> canciones;
+    private long unidadesVendidasUltMes;
+    private Set<Cancion> canciones;
+
+    public Disco(long unidadesVendidasUltMes){
+        this.unidadesVendidasUltMes=unidadesVendidasUltMes;
+        canciones = new HashSet<>();
+    }
+
+    public long getUnidadesVendidasUltMes(){return unidadesVendidasUltMes;}
+
+    public void addCancion(Cancion cancion){
+        if(!canciones.add(cancion)){
+            canciones.add(cancion);
+        }
+    }
+
+    public long getReproduccionesDisco(){
+        long total=0;
+        Iterator<Cancion> iterator = canciones.iterator();
+        while(iterator.hasNext()){
+            Cancion cancion = iterator.next();
+            total+=cancion.getCantReprodUltMes();
+        }
+        return total;
+    }
+
+
+
+    public String toString(){
+        String retorna= "Disco\nUnidades Vendidas en el último mes: "+unidadesVendidasUltMes+"\nCanciones del disco: \n";
+        Iterator<Cancion> iterator = canciones.iterator();
+        while(iterator.hasNext()){
+            Cancion cancion = iterator.next();
+            retorna+="\n"+cancion.toString();
+        }
+        return retorna;
+    }
+
 }
