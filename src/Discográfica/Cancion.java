6d216119aab1628c6daea2ed083da4d7f8237874
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-18 14:38:26 -0300

    Update Cancion.java

--- src/Discográfica/Cancion.java
+++ src/Discográfica/Cancion.java
@@ -3,5 +3,17 @@ package Discográfica;
 public class Cancion {
     private String nombre;
     private String duracion; //string o que??? porq dice minutos y segundos, puede ser 3:23
-    private long int cantReprodUltMes;
+    private long cantReprodUltMes;
+
+    public Cancion(String nombre, String duracion, long cantReprodUltMes){
+        this.cantReprodUltMes=cantReprodUltMes;
+        this.nombre=nombre;
+        this.duracion=duracion;
+    }
+
+    public long getCantReprodUltMes(){return cantReprodUltMes;}
+
+    public String toString(){
+        return "Cancion: "+nombre+" Duracion: "+duracion+" Reproducciones en el Mes: "+cantReprodUltMes;
+    }
 }
