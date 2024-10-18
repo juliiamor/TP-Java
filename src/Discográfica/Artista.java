76151293dd64499ed9559ef5468c24c5f33668e2
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-18 14:36:05 -0300

    Añado metodos y cambio contenedor de Artista.java
    Cambio contenedor List a Set para que no se puedan repetir ni recitales ni discos. Añado los primeros metodos

--- src/Discográfica/Artista.java
+++ src/Discográfica/Artista.java
@@ -1,14 +1,62 @@
 package Discográfica;
-import
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public abstract class Artista {
     private String identificador;
     private String nombre;
-    private int cantIntegrantes;
+    private byte cantIntegrantes;
     private String generoMusical;
-    private List<Disco> discos;
-    private List<Recital> recitales;
-}
+    private Set<Disco> discos;
+    private Set<Recital> recitales;
 
+    public Artista(String identificador,String nombre,byte cantIntegrantes,String generoMusical){
+        this.identificador=identificador;
+        this.nombre=nombre;
+        this.cantIntegrantes=cantIntegrantes;
+        this.generoMusical=generoMusical;
+        discos = new HashSet<>();
+        recitales = new HashSet<>();
+    }
+
+    public String getIdentificador(){return identificador;}
+
+    public String getNombre(){return nombre;}
+
+    public byte getCantIntegrantes(){return cantIntegrantes;}
+
+    public String getGeneroMusical(){return generoMusical;}
+
+    public void addDisco(Disco disco){
+        if(!discos.add(disco)){
+            discos.add(disco);
+        }
+    }
+
+    public void addRecital(Recital recital){
+        if(!recitales.add(recital)){
+            recitales.add(recital);
+        }
+    }
+
+    public void listadoUnidadesVendidasPorDisco(){
+        long acumulaVentas=0;
+        byte cantDiscos=0;
+        float promedio;
+        Iterator<Disco> iterator = discos.iterator();
+        System.out.println("\nListado de Ventas por disco en el Mes\n");
+        while(iterator.hasNext()){
+            Disco disco = iterator.next();
+            System.out.println(disco.toString());
+            acumulaVentas+=disco.getUnidadesVendidasUltMes();
+            cantDiscos++;
+        }
+        if(cantDiscos!=0){
+            promedio=acumulaVentas/(float)cantDiscos;
+            System.out.println("\nPromedio de ventas en el mes: "+promedio);
+        }
+    }
+
+}
