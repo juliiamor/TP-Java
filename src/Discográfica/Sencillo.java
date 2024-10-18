73b4fb55cf0db724a43ce2c3b96b88720cd1478d
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-18 14:40:27 -0300

    Añado Constructor

--- src/Discográfica/Sencillo.java
+++ src/Discográfica/Sencillo.java
@@ -1,4 +1,7 @@
 package Discográfica;
 
 public class Sencillo extends Cancion {
+    public Sencillo(String nombre, String duracion, long cantReprodUltMes){
+        super(nombre, duracion, cantReprodUltMes);
+    }
 }
