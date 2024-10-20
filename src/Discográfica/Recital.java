bd09f62c61b131467924cf79ba031ffa664aa99c
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-19 22:56:20 -0300

    Metodos basicos

--- src/Discográfica/Recital.java
+++ src/Discográfica/Recital.java
@@ -4,4 +4,16 @@ public class Recital {
     private String fecha;
     private float recaudacion;
     private float costosProduccion;
+
+    public Recital(String fecha, float recaudacion,float costosProduccion){
+        this.costosProduccion=costosProduccion;
+        this.fecha=fecha;
+        this.recaudacion=recaudacion;
+    }
+
+    public float getNeto(){return recaudacion-costosProduccion;}
+
+    public String toString() {
+        return "Fecha: "+fecha+" Recaudacion: "+recaudacion+" Costos de Produccion: "+costosProduccion+" Ingresos netos: "+getNeto();
+    }
 }
