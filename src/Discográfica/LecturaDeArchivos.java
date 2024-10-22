dd2d0f257f83a37333c08741e316abbebe86ef88
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-21 22:33:14 -0300

    Primera Serializacion (No se si esta bien)

--- src/Discográfica/LecturaDeArchivos.java
+++ src/Discográfica/LecturaDeArchivos.java
@@ -1,4 +1,33 @@
 package Discográfica;
+import java.io.*;
+import java.util.Iterator;
+import java.util.TreeMap;
 
-public class LecturaDeArchivos {
+public abstract class LecturaDeArchivos {
+
+    public static void cargarArtistas(TreeMap<String, Artista> artistas) {
+        try (ObjectInputStream ois=new ObjectInputStream(new FileInputStream("src/Discográfica/Archivos/artistasSerializado.dat"))) {
+            while (true) {
+                Artista artista=(Artista) ois.readObject();
+                artistas.put(artista.getIdentificador(), artista);
+            }
+        } catch (EOFException e) {
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al cargar artistas: "+e.getMessage());
+        }
+    }
+
+    public static void guardarArtistas(TreeMap<String, Artista> artistas) {
+        try (ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("src/Discográfica/Archivos/artistasSerializado.dat"))) {
+            Iterator<Artista> iterator=artistas.values().iterator();
+            Artista artista;
+            while(iterator.hasNext()){
+                artista = iterator.next();
+                oos.writeObject(artista);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al guardar artistas: "+e.getMessage());
+        }
+    }
 }
