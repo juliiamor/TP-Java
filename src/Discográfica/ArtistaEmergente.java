e20e230963f43debae3cc0fa0a4ad4fa07568692
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-21 22:41:16 -0300

    toString agregado

--- src/Discográfica/ArtistaEmergente.java
+++ src/Discográfica/ArtistaEmergente.java
@@ -1,7 +1,11 @@
 package Discográfica;
-
 public class ArtistaEmergente extends Artista{
     public ArtistaEmergente(String identificador,String nombre,byte cantIntegrantes,String generoMusical){
         super(identificador, nombre, cantIntegrantes, generoMusical);
+    }
+
+    @Override
+    public String toString() {
+        return "ArtistaEmergente - "+super.toString();
     }
 }
