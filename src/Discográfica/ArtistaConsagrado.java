48f67e82a079b57c3004b8f3cf8699c18ea7c385
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-21 22:40:52 -0300

    toString agregado

--- src/Discográfica/ArtistaConsagrado.java
+++ src/Discográfica/ArtistaConsagrado.java
@@ -1,7 +1,10 @@
 package Discográfica;
-
 public class ArtistaConsagrado extends Artista{
     public ArtistaConsagrado(String identificador,String nombre,byte cantIntegrantes,String generoMusical){
         super(identificador, nombre, cantIntegrantes, generoMusical);
+    }
+
+    public String toString() {
+        return "ArtistaConsagrado - "+super.toString();
     }
 }
