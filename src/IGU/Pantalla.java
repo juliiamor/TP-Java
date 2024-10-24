1b6d795afd2fe6ff776830d9e8851dbcee58e15e
juliiamor <juliiamor17@gmail.com>
2024-10-24 16:38:38 -0300

    interfaz top 10

--- src/IGU/Pantalla.java
+++ src/IGU/Pantalla.java
@@ -52,6 +52,18 @@ public class Pantalla extends JFrame {   // Hereda de JFrame
 
             filtrarArtistasPorGeneroYIntegrantes(generoSeleccionado, cantidadIntegrantes);
         });
+        top10Button.addActionListener(e -> {
+            String genero = textField5.getText().trim();
+            mostrarTop10(genero);
+        });
+        discosButton.addActionListener( e -> {
+            String identificador = textField6.getText().trim();
+            mostrarUnidadesVendidas(identificador);
+        });
+
+        artistasButton.addActionListener(e -> {
+            mostrarListadoArtistas();
+        });
     }
 
     private void createUIComponents() {
@@ -88,5 +100,46 @@ public class Pantalla extends JFrame {   // Hereda de JFrame
 
         // Mostrar la ventana con la liquidación
         JOptionPane.showMessageDialog(this, scrollPane, "Liquidación del Artista", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void mostrarTop10(String genero){
+        try{
+            GeneroMusical generoMusical=GeneroMusical.valueOf(genero.toUpperCase());
+
+            JTextArea textArea = new JTextArea(Reportes.Top10CancionesPorGenero(gestion.getArtistas(),generoMusical));
+            textArea.setEditable(false);
+            textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(600,400));
+
+            String titulo = "Top 10 canciones del género "+genero;
+            JOptionPane.showMessageDialog(this,scrollPane,titulo,JOptionPane.INFORMATION_MESSAGE);
+
+        }catch (IllegalArgumentException e){
+            JOptionPane.showMessageDialog(this, "El artista ingresado no existe", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void mostrarListadoArtistas(){
+        //
+    }
+
+    public void mostrarUnidadesVendidas(String identificador){
+        try{
+            JTextArea textArea = new JTextArea(Reportes.unidadesVendidas(identificador,gestion.getArtistas()));
+            textArea.setEditable(false);
+            textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(600,400));
+
+            String titulo = "Unidades de Discos vendidas en el mes del Artista "+identificador;
+            JOptionPane.showMessageDialog(this,scrollPane,titulo,JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (IllegalArgumentException e){
+            JOptionPane.showMessageDialog(this, "El artista ingresado no existe", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 }
