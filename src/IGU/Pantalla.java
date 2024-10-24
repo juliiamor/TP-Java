26a03aa9d38fb5dbd059f9e49a06a2cb29b34202
gerogg29 <geronimoggarcia2018@gmail.com>
2024-10-23 22:37:13 -0300

    Agregue los listados de MostrarUnidadesVendidas y Top10 para que puedan mostrarse en la pantalla

--- src/IGU/Pantalla.java
+++ src/IGU/Pantalla.java
@@ -1,6 +1,8 @@
 package IGU;
 
+import Discográfica.GeneroMusical;
 import Discográfica.Gestion;
+import Discográfica.Reportes;
 
 import javax.swing.*;
 import java.awt.*;
@@ -33,6 +35,14 @@ public class Pantalla extends JFrame {   // Hereda de JFrame
             String identificador = textField1.getText().trim();
             mostrarLiquidacion(identificador);
         });
+        top10Button.addActionListener(e -> {
+            String genero = textField5.getText().trim();
+            mostrarTop10(genero);
+        });
+        discosButton.addActionListener( e -> {
+            String identificador = textField6.getText().trim();
+            mostrarUnidadesVendidas(identificador);
+        });
     }
 
     private void createUIComponents() {
@@ -58,5 +68,40 @@ public class Pantalla extends JFrame {   // Hereda de JFrame
         JOptionPane.showMessageDialog(this, scrollPane, "Liquidación del Artista", JOptionPane.INFORMATION_MESSAGE);
     }
 
-}
+    public void mostrarTop10(String genero){
+        try{
+            GeneroMusical generoMusical=GeneroMusical.valueOf(genero.toUpperCase());
 
+            JTextArea textArea = new JTextArea(Reportes.Top10CancionesPorGenero(gestion.getArtistas(),generoMusical));
+            textArea.setEditable(false);
+            textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(600,400));
+
+            String titulo = "Top 10 canciones del género "+genero;
+            JOptionPane.showMessageDialog(this,scrollPane,titulo,JOptionPane.INFORMATION_MESSAGE);
+
+        }catch (IllegalArgumentException e){
+            JOptionPane.showMessageDialog(this, "El artista ingresado no existe", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void mostrarUnidadesVendidas(String identificador){
+        try{
+            JTextArea textArea = new JTextArea(Reportes.unidadesVendidas(identificador,gestion.getArtistas()));
+            textArea.setEditable(false);
+            textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(600,400));
+
+            String titulo = "Unidades de Discos vendidas en el mes del Artista "+identificador;
+            JOptionPane.showMessageDialog(this,scrollPane,titulo,JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (IllegalArgumentException e){
+            JOptionPane.showMessageDialog(this, "El artista ingresado no existe", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+}
